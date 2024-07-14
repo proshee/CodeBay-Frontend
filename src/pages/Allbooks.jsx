35949a028dad90d54e9e282cd73298cd3bc142d8
Tanuj <tanujchandela@gmail.com>
2024-07-14 18:45:17 +0530

    Loader fixed for ALL Books Section

--- src/pages/Allbooks.jsx
+++ src/pages/Allbooks.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 
 
 const Allbooks = () => {
-    const [Books, setBooks] = useState([]);
+    const [Books, setBooks] = useState();
     useEffect(() => {
       const fetch = async () => {
         const res = await axios.get("https://codebay.onrender.com/api/v1/get-all-books");
@@ -19,7 +19,7 @@ const Allbooks = () => {
     <div className="bg-slate-300 h-auto px-12 py-8">
         <h4 className="text-3xl text-gray-600 text-center">All Books</h4>
         {!Books && (
-          <div className="flex items-center justify-center my-8">
+          <div className="flex h-screen items-center justify-center my-8">
             <Loader />
           </div>
         )}
